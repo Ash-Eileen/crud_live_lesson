9ea59eecdcddc3c29000729a3f216e17915236cf
Ashley Smith <ash.mash.134@gmail.com>
2020-10-06 09:09:35 +1100

    Updated files

--- server/app.js
+++ server/app.js
@@ -1,6 +1,6 @@
 const express = require("express")
-const supertest = require("supertest")
 const app = express()
+const postRouter = require("./routes/post_routes")
 const port = 3000
 
 // parse application/x-www-form-urlencoded
@@ -15,6 +15,18 @@ app.get('/', (req, res) => {
         message:"Hello World!"
     })
 })
+
+app.post('/studentNames', (request, response) => {
+    let incomingData = request.body.studentNames;
+    console.log(incomingData);
+    response.json({
+        firstStudentName: incomingData[0]
+    })
+})
+
+// any routes starting /posts must be redirected to post_routes file
+// ensure its exported
+app.use("/posts", postRouter)
 
 const server = app.listen(port, () => {
     console.log(`Blog express app listening on port ${port}`)
